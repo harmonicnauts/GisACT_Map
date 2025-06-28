9464f2e23534c90a1c2cf2c8187517ce079d874c
Nycht1 <nasutionrachmat@gmail.com>
2025-06-28 12:39:06 +0700

    update metadata

--- gisact-map/src/components/TopBar/index.tsx
+++ gisact-map/src/components/TopBar/index.tsx
@@ -2,8 +2,6 @@ export default function TopBar() {
   return (
     <header className="fixed top-0 left-0 w-full h-14 bg-gray-800 text-white z-[1001] shadow-md flex items-center justify-between px-6">
       <h1 className="text-lg font-bold">🚮 Trash GeoVisualization</h1>
-      <nav>
-      </nav>
     </header>
   );
 }
